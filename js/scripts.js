0d86531c25fb45656d2992924dfbbbf3526fe3c6
Erica Wright and Patrick McGreevy <patrick@>
2017-01-10 10:09:55 -0800

    Add Bootstrap and controls for background color

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,12 @@ $(document).ready(function() {
   $("#toggle").click(function() {
     $("img.poster").slideToggle();
   });
+  $("button#black-on-white").click(function() {
+    $("body").removeClass();
+    $("body").addClass("light");
+  });
+  $("button#white-on-black").click(function() {
+    $("body").removeClass();
+    $("body").addClass("dark");
+  });
 });
